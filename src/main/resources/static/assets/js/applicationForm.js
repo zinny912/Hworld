71b39648b7e5263698b468e7cc84e5804422c11f
YU3727 <lacvert3727@naver.com>
2023-06-05 01:14:48 +0900

    applicationForm
    230605

--- src/main/resources/static/assets/js/applicationForm.js
+++ src/main/resources/static/assets/js/applicationForm.js
@@ -3,6 +3,19 @@ $(function(){
 	console.log('applicationForm Page');
 })
 
+//폼 넘길때 데이터가 잘 넘어가는지 확인하고자 할 때 쓰는 것
+//let $frm = $('#appForm').serialize();
+//alert($frm);
+
+
+//가입하기 버튼 눌렀을 때
+$('#completeForm').click(function(){
+    console.log('가입하기 버튼');
+    let $frm = $('#appForm').serialize();
+    alert($frm);
+    $('#appForm').submit();
+})
+
 
 //가입유형 확인
 //신규가입 버튼
@@ -46,8 +59,26 @@ $('#joinType3').click(function(){
 
 //요금제 선택 확인
 $('#planArea').on("click", function(){
-    console.log('event 위임');
-    let plan = $('input[name="flexRadioDefault"]:checked').val();
-    $('#billPlan').text(plan);
+    let planVal = $('input[name="planNum"]:checked').val();
+    let planTxt = $('input[name="planNum"]:checked').siblings('label').text();
+    //영수증 표시값 변경
+    $('#billPlan').text(planTxt);
+    //input tag에 값 넣기
+})
 
+
+//할인 유형 선택 확인
+$('#discountArea').on("click", function(){
+    let disKindVal = $('input[name="disKind"]:checked').val();
+    let disKindAllTxt = $('input[name="disKind"]:checked').siblings('label').text(); //전체 label text
+
+    //span을 제외한 label text
+    let disKindTxt = $('input[name="disKind"]:checked').siblings('label')     // label 요소를 복제합니다
+                                                        .children('span')    // span 요소를 선택합니다
+                                                        .remove()            // 선택한 span 요소를 제거합니다
+                                                        .end()               // 원래의 복제된 요소로 돌아갑니다
+                                                        .text();             // label 요소의 텍스트를 가져옵니다
+    $('#billDisKind').text(disKindTxt);
+
+    //input tag에 값 넣기
 })
