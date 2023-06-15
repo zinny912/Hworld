7a8eb9cfd5b5fe800c1fe6a11472dd4d16ba09d0
YU3727 <lacvert3727@naver.com>
2023-06-15 15:48:00 +0900

    accessoryDetail
    230615

--- src/main/resources/static/assets/js/accessoryDetail2.js
+++ src/main/resources/static/assets/js/accessoryDetail2.js
@@ -36,7 +36,6 @@ for (let j=0; j < slicedCodes.length; j++) {
 //colorCode list 작업 끝
 
 
-
 });
 //$(document).ready(function(){}) 끝
 
@@ -44,6 +43,7 @@ for (let j=0; j < slicedCodes.length; j++) {
 //전역변수
 let idx=0;
 let arrPrice = [];
+
 
 //색상 옵션이 선택되었을 때 이벤트
 $('#colorTypes').on('click', 'li[name="colorCode"]', function(e){
@@ -58,7 +58,6 @@ $('#colorTypes').on('click', 'li[name="colorCode"]', function(e){
     //옵션 이름 표시 함수 호출
     setColorName(selectedColor);
     
-    
     //해당 컬러코드(selectedColor)를 포함하는 directCode를 id로 쓰는 div 태그 선택
     $('.titlebox').each(function(){
       let directCode = $(this).attr('id');
@@ -70,7 +69,6 @@ $('#colorTypes').on('click', 'li[name="colorCode"]', function(e){
         $('.titlebox[data-selected=0]').hide();
         $('.titlebox[data-selected=1]').show();
         //색상 선택시 옵션창에 색상의 가격이 들어가게 해줌
-        console.log('색상 일치! 일치하는 코드는: ', directCode);
         setOptionPrice(directCode);
         //setDirValue(directCode);
         return false;
@@ -109,12 +107,6 @@ $('#optionAdd').on('click', function() {
   let optionPrice = $('#optionPrice').val();
   let optionAmount = $('#optionQuantity').val();
   let calPrice = optionPrice*optionAmount;
-
-  console.log("directCode", directCode);
-  console.log("optionColor", optionColor);
-  console.log("optionPrice", optionPrice);
-  console.log("optionAmount", optionAmount);
-  console.log("calculationPrice", calPrice);
   
   //값 존재여부 확인
   codeCheck = isEmpty(directCode);
@@ -172,15 +164,13 @@ $('#optionAdd').on('click', function() {
 //추가 버튼 클릭 시 종료
 
 
-// x 버튼 클릭 시 옵션 삭제
+//x 버튼 클릭 시 옵션 삭제
 $(document).on('click', '.fa-times', function() {
   //x버튼과 optionOne을 연동, 해당 optionOne 삭제
   let idx = $(this).attr('data-idx');
-  console.log(idx);
   $('#optionOne'+idx).remove();
-  //$(this).closest('.cart-content-wrap').remove();
 
-  //totalPrice 계산을 위한 배열 호출, 배열의 해당 idx값 삭제
+  //totalPrice 계산을 위한 배열 호출, 배열의 해당 idx값에 0 입력
   let ar = arrPrice;
   ar[idx] = 0;
 
@@ -196,10 +186,6 @@ $(document).on('click', '.fa-times', function() {
 });
 
 
-
-
-
-
 //필요한 값이 입력되어있는지 확인하는 함수
 function isEmpty(value){
   if(typeof value == "undefined" || value == null || value == '')
@@ -209,9 +195,7 @@ function isEmpty(value){
 }
 
 
-
-
-//색상 옵션이 선택됐을 때 옵션에 따른 개당 가격을 세팅해주는 함수
+//색상 옵션이 선택됐을 때 옵션에 따른 개당 가격, 재고를 세팅해주는 함수
 function setOptionPrice(directCode){
   let item = $('#item_'+directCode);
   let initPrice = item.attr("data-direct-price");
@@ -223,11 +207,6 @@ function setOptionPrice(directCode){
 }
 
 
-
-
-
-
-
 //색상 옵션이 선택됐을 때 값을 넣어주는 함수
 function setDirValue(directCode){
   let item = $('#item_'+directCode);
@@ -237,8 +216,6 @@ function setDirValue(directCode){
   console.log(item.attr("data-sliced-code"));
   console.log(item.attr("data-direct-stock"));
   console.log(item.attr("data-direct-price"));
-
-
 }
 
 
@@ -258,252 +235,4 @@ function setColorName(selectedColor){
     $('#colorName').val(colorName);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//일치하는 directCode를 가진 titlebox를 표시하고 나머지는 숨김처리
-// function updateTitleBoxVisibility(directCode) {
-// const directListItems = $('#directList').find('.direct-item');
-// let matchingItemFound = false;
-// //loop
-// directListItems.each(function() {
-// const listItem = $(this);
-// const listItemDirectCode = listItem.attr('data-direct-code');
-
-// if (listItemDirectCode === directCode) {
-// listItem.closest('.titlebox').show();
-// matchingItemFound = true;
-// const price = $(this).data('direct-price');
-// // 수량 버튼으로 orderAmount, totalPrice 값 만들기
-// $('.quantity-left-minus').click(function(e) {
-// e.preventDefault();
-// e.stopPropagation();
-// const inputNumber = $(this).siblings('.input-wrapper').find('.input-number');
-// console.log(inputNumber.val());
-// let quantity = parseInt(inputNumber.val());
-
-// if (quantity > 1) {
-// quantity=quantity-1;
-// inputNumber.val(quantity);
-// calculateTotalPrice();
-// }
-// });
-
-// // 수량 증가 버튼 클릭 시
-// $('.quantity-right-plus').click(function(e) {
-// e.preventDefault();
-// e.stopPropagation();
-// const inputNumber = $(this).siblings('.input-wrapper').find('.input-number');
-// let quantity = parseInt(inputNumber.val());
-
-// quantity=quantity+1;
-// inputNumber.val(quantity);
-// calculateTotalPrice();
-// });
-
-// // 수량 변경 시
-// $('.input-number').change(function() {
-// calculateTotalPrice();
-// });
-
-// function calculateTotalPrice() {
-// let initPrice=price;
-// const quantity2= $('.input-number').val();
-// const totalPrice=initPrice*quantity2;
-
-// console.log("total Price: ", totalPrice);
-
-// $('#totalQty').text(totalPrice);
-// $('#totalPrice').val(totalPrice);
-// $('#orderAmount').val(quantity2);
-// }
-// } else {
-// listItem.closest('.titlebox').hide();
-// }
-
-// //loop end
-// });
-
-// if (!matchingItemFound) {
-//   $('.noStock').show(); // 일치하는 제품이 없는 경우 재고없음을 나타내는 div를 보여줌
-//   } else {
-//   $('.noStock').hide(); // 일치하는 제품이 있는 경우 재고없음을 나타내는 div를 숨김
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////
-
-// Color Option 선택시 directCode 만들기 완성된 것을 $('#directCode').val에 저장
-// $('.optionArea').on('click', 'li[name="colorCode"]', function(e) {
-//   e.preventDefault();
-//   const selectedOptions = getSelectedOptions();
-
-//   let colorCode = selectedOptions.colorCode;
-//   let categoryCode = $("#categoryCode").val();
-//   let brandCode = $("#brandCode").val();
-//   let slicedCode = $("#slicedCode").val();
-//   let directCode = '';
-//   if (colorCode != null) {
-//     directCode = "P" + categoryCode + "B" + brandCode + "C" + colorCode + "V000" + slicedCode;
-//     $('#directCode').text(directCode);
-//     updateTitleBoxVisibility(directCode); // 일치하는 titlebox 업데이트
-    
-//     // 선택된 옵션 이름 수량 옆 버튼에 보여주기
-//     const colorValue=$('#colorName').val('C'+colorCode).val();
-//     if(colorValue==="CW"){
-// 	  const colorName = "화이트";
-// 	  $('#colorName').val(colorName);
-//   }else if(colorValue==="CB"){
-// 	  const colorName = "블랙";
-// 	  $('#colorName').val(colorName);
-//   }else if(colorValue==="CG"){
-// 	  const colorName = "그레이";
-// 	  $('#colorName').val(colorName);
-//   }else{
-// 	  const colorName ="옵션없음";
-// 	  $('#colorName').val(colorName);
-//   }
-
-//   }
-//   e.stopPropagation();
-
-// // 일치하는 directCode를 가진 titlebox를 표시하고 나머지는 숨김
-// function updateTitleBoxVisibility(directCode) {
-// const directListItems = $('#directList').find('.direct-item');
-// let matchingItemFound = false;
-// //loop
-// directListItems.each(function() {
-//   const listItem = $(this);
-//   const listItemDirectCode = listItem.attr('data-direct-code');
-
-//   if (listItemDirectCode === directCode) {
-//     listItem.closest('.titlebox').show();
-//     matchingItemFound = true;
-// 	const price = $(this).data('direct-price');
-//     // 수량 버튼으로 orderAmount, totalPrice 값 만들기
-//     $('.quantity-left-minus').click(function(e) {
-//       e.preventDefault();
-//       e.stopPropagation();
-//       const inputNumber = $(this).siblings('.input-wrapper').find('.input-number');
-//       console.log(inputNumber.val());
-//       let quantity = parseInt(inputNumber.val());
-
-//       if (quantity > 1) {
-//         quantity=quantity-1;
-//         inputNumber.val(quantity);
-//         calculateTotalPrice();
-//       }
-//     });
-
-//     // 수량 증가 버튼 클릭 시
-//     $('.quantity-right-plus').click(function(e) {
-//       e.preventDefault();
-//       e.stopPropagation();
-//       const inputNumber = $(this).siblings('.input-wrapper').find('.input-number');
-//       let quantity = parseInt(inputNumber.val());
-
-//       quantity=quantity+1;
-//       inputNumber.val(quantity);
-//       calculateTotalPrice();
-//     });
-
-//     // 수량 변경 시
-//     $('.input-number').change(function() {
-//     calculateTotalPrice();
-//     });
-
-//    function calculateTotalPrice() {
-//     let initPrice=price;
-//       const quantity2= $('.input-number').val();
-//       const totalPrice=initPrice*quantity2;
-
-// 	console.log("total Price: ", totalPrice);
-
-// 	$('#totalQty').text(totalPrice);
-// 	$('#totalPrice').val(totalPrice);
-// 	$('#orderAmount').val(quantity2);
-//    }
-//   } else {
-//     listItem.closest('.titlebox').hide();
-//   }
-  
-// //loop end
-// });
-
-// if (!matchingItemFound) {
-  
-//   $('.noStock').show(); // 일치하는 제품이 없는 경우 재고없음을 나타내는 div를 보여줌
-// } else {
-//   $('.noStock').hide(); // 일치하는 제품이 있는 경우 재고없음을 나타내는 div를 숨김
-// }  
-
-// }
-
-// function getSelectedOptions(e) {
-  
-// 	const options = {
-// 	  colorCode: null,
-// 	};
-// 	const colorCode = $('.optionArea li[name="colorCode"].selected').attr('value');
-
-// 	if (colorCode) {
-//   	options.colorCode = colorCode;
-// 	}
-// 	return options;
-// 	}
-  
-// }); 
-
-//   // 후기 작성 버튼 클릭 시 모달 창이 열릴 때 실행되는 함수
-//    $('#addReview').on('show.bs.modal', function (event) {
-//      const directName = $('.directNameValue').data('direct-name'); // 해당 후기 작성 버튼에 연결된 제품의 directName 값을 가져옴
-//      $('#directName2').text(directName); // 모달 창 내에서 제품명을 표시하는 곳에 directName 값을 설정
-//    });
-//    //문의 작성 버튼 클릭 시 모달 창이 열릴 때 실행되는 함수
-//    $('#addQna').on('show.bs.modal', function (event) {
-//          const directName = $('.directNameValue').data('direct-name'); // 해당 후기 작성 버튼에 연결된 제품의 directName 값을 가져옴
-//          $('#directName3').text(directName); // 모달 창 내에서 제품명을 표시하는 곳에 directName 값을 설정
-//        });
-    
-//    $('#addReply').on('show.bs.modal', function(event) {
-//        const button = $(event.relatedTarget); // 클릭한 버튼 요소
-//        const qnaNum = button.data('qna-num'); // data-qna-num 속성 값 가져오기
-//        const qnaContents = button.closest('.accordion-item').find('#qnaContentsQ').text().trim(); // 문의 내용 가져오기
-//        const directName = $('.directNameValue').data('direct-name'); // 해당 후기 작성 버튼에 연결된 제품의 directName 값을 가져옴
-//          $('#directName4').text(directName); // 모달 창 내에서 제품명을 표시하는 곳에 directName 값을 설정
-  
-//        // 모달 내부 요소에 값 설정
-//        $(this).find('#modalQnaNum').val(qnaNum);
-//        $(this).find('#modelQnaContents').val(qnaContents);
-//      });
-  
 
